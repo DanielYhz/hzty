0139a3682677b683580c17bc475e811226c75979
Daniel Yang <yhzdaniel@gmail.com>
2018-11-11 20:08:23 -0800

    show pictures

--- index.php
+++ index.php
@@ -11,27 +11,46 @@
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
-
+                
+                <?php
+                
+         $query = "SELECT * FROM products";
+         $select_all_products_query = mysqli_query($connection, $query);                    
+        
+        while ($row = mysqli_fetch_assoc($select_all_products_query)) {
+            $product_area = $row['product_area'];
+            $product_post_author = $row['product_post_author'];
+            $product_date = $row['product_date'];
+            $product_image = $row['product_image'];
+            $product_description = $row['product_description'];
+            
+            ?>
+            
                 <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
+                    The Housing Zenith Talented Yard
+                    <small>Post Your House</small>
                 </h1>
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#">Blog Post Title</a>
+                    <a href="#"><?php echo $product_area ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                    by <a href="index.php"><?php echo $product_post_author ?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $product_date ?> </p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="images/<?php echo $product_image; ?>" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                <p><?php echo $product_description ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                <hr>
+                <hr>            
+   
+  <?php } ?>
+                
+
+
                 
             </div>
 
